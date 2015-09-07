f6ea13d103db692a5ec5577ecc35272f29c8141a
Lukas Hermanns <lhlukash23@googlemail.com>
2015-09-07 15:45:13 +0200

    Added STB image write lib to test file.

--- test/test1.cpp
+++ test/test1.cpp
@@ -8,7 +8,31 @@
 #include <Typo/Typo.h>
 #include <iostream>
 
+#pragma warning(disable : 4996)
+#define STB_IMAGE_WRITE_IMPLEMENTATION
+#include "stb_image_write.h"
+
 using namespace Tg;
+
+void drawPixel(unsigned char c)
+{
+    const char table[] = { ' ', '.', ',', '+', '*', 'o', 'a', '@' };
+    std::cout << table[c >> 5];
+}
+
+void drawImage(const Image& image)
+{
+    auto pixels = image.GetImageBuffer().begin();
+    for (unsigned int y = 0; y < image.GetSize().height; ++y)
+    {
+        for (unsigned int x = 0; x < image.GetSize().width; ++x)
+        {
+            drawPixel(*pixels);
+            ++pixels;
+        }
+        std::cout << std::endl;
+    }
+}
 
 int main()
 {
@@ -18,15 +42,26 @@ int main()
     FontDescription fontDesc;
     FontGlyphRange glyphRange;
 
-    glyphRange.first   = 0x53F2;//'A';
-    glyphRange.last    = 0x53FF;//'z';
+    #if 0
+    glyphRange.first   = 0x53F2;
+    glyphRange.last    = 0x53F4;
+    #else
+    glyphRange.first   = 32;//'A';
+    glyphRange.last    = 255;//'z';
+    #endif
     
-    fontDesc.height = 32;
+    fontDesc.height = 60;//32;
     fontDesc.name   = "C:/Windows/Fonts/kaiu.ttf";
 
     try
     {
-        Font font(fontDesc, glyphRange);
+        auto fontModel = BuildFont(fontDesc, glyphRange);
+
+        Font font(fontDesc, std::move(fontModel.glyphSet));
+
+        //drawImage(fontModel.image);
+        const auto& img = fontModel.image;
+        stbi_write_png("font_atlas.png", img.GetSize().width, img.GetSize().height, 1, img.GetImageBuffer().data(), img.GetSize().width);
 
         MultiLineString<char> mlStr(font, 100, "test");
 
