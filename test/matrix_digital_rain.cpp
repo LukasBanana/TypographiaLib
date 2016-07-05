9c02e0b8f5327be66507a5e171c306f4648109ff
Lukas Hermanns <lhlukash23@googlemail.com>
2016-07-05 10:49:52 +0200

    Renamed "test3" to "matrix_digital_rain".

--- test/matrix_digital_rain.cpp
+++ test/matrix_digital_rain.cpp
@@ -1,5 +1,5 @@
 /*
- * test3.cpp
+ * matrix_digital_rain.cpp
  * 
  * This file is part of the "TypographiaLib" project (Copyright (c) 2015 by Lukas Hermanns)
  * See "LICENSE.txt" for license information.
